c847bd409e5071676ade3d19f47af88c256f1f0b
Vlad <vladmdgolam@ya.ru>
2024-01-15 00:08:11 +0100

    Update Scene with rect support and styles

--- app/Scene.tsx
+++ app/Scene.tsx
@@ -2,15 +2,16 @@ interface SVGElementProperties {
   cx?: number
   cy?: number
   r?: number
+  width?: number
+  height?: number
   fill?: string
-  tagName: string
-  type: string
+  x?: number // Added for rect elements
+  y?: number // Added for rect elements
 }
-
 interface SVGElement {
   children: SVGElement[] // Assuming children could also be SVG elements
   properties: SVGElementProperties
-  tagName: string
+  tagName: "circle" | "rect"
   type: string
 }
 
@@ -19,15 +20,19 @@ export type Shapes = SVGElement[]
 export const Scene = ({ shapes }: { shapes: Shapes }) => {
   return (
     <svg
-      width="720"
-      height="124"
+      className="w-full h-full"
       viewBox="0 0 720 124"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       {shapes.map((shape, index) => {
-        const { cx, cy, r, fill, tagName } = shape.properties
-        return <circle key={index} cx={cx} cy={cy} r={r} fill={"white"} />
+        const { cx, cy, r, width, height, fill, x, y } = shape.properties
+        const { tagName } = shape
+        if (tagName === "circle") {
+          return <circle key={index} cx={cx} cy={cy} r={r} fill="white" />
+        } else if (tagName === "rect") {
+          return <rect key={index} x={x} y={y} width={width} height={height} fill="white" />
+        }
       })}
     </svg>
   )
