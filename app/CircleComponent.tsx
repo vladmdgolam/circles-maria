93e0b3d00b4edb205781214d32abd269029f722d
Vlad <vladmdgolam@ya.ru>
2024-01-15 00:25:08 +0100

    Add animation to CircleComponent with framer-motion

--- app/CircleComponent.tsx
+++ app/CircleComponent.tsx
@@ -1,5 +1,14 @@
+import { useAnimationFrame } from "framer-motion"
+import { useRef } from "react"
+
 import { SVGElementProperties } from "./Scene"
 
-export const CircleComponent = ({ cx, cy, r, fill }: SVGElementProperties) => (
-  <circle cx={cx} cy={cy} r={r} fill={fill} />
-)
+export const CircleComponent = ({ cx, cy, r, fill }: SVGElementProperties) => {
+  const ref = useRef<SVGCircleElement>(null)
+  useAnimationFrame((time, delta) => {
+    if (!ref.current) return
+    ref.current.style.transform = `rotateY(${time}deg)`
+  })
+
+  return <circle cx={cx} cy={cy} r={r} fill={fill} ref={ref} />
+}
