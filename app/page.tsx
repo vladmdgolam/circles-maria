1d43e2d75481b81937105c7a228db8652b67645f
Vlad <vladmdgolam@ya.ru>
2024-01-14 23:45:24 +0100

    Add type for parsed SVG and log shapes

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,20 @@ import { parse } from "svg-parser"
 
 import maria from "./maria.svg"
 
+interface ParsedSVG {
+  children: {
+    children?: any[] // Replace `any` with a more specific type if possible
+  }[]
+}
+
 export default function Home() {
-  const svg = parse(maria)
+  const svg = parse(maria) as ParsedSVG
 
   useEffect(() => {
-    const shapes = svg.children[0]
+    if (svg.children[0].children && svg.children[0].children.length > 0) {
+      const shapes = svg.children[0].children
+      console.log(shapes)
+    }
   }, [])
 
   return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
