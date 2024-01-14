47b5d090c4a0bde78b887c0641364b005c47d63e
Vlad <vladmdgolam@ya.ru>
2024-01-14 23:52:38 +0100

    Add Scene component and 3D libraries

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { Canvas } from "@react-three/fiber"
 import { useEffect } from "react"
 import { parse } from "svg-parser"
 
 import maria from "./maria.svg"
+import { Scene } from "./Scene"
 
 interface ParsedSVG {
   children: {
@@ -17,9 +19,14 @@ export default function Home() {
   useEffect(() => {
     if (svg.children[0].children && svg.children[0].children.length > 0) {
       const shapes = svg.children[0].children
-      console.log(shapes)
     }
   }, [])
 
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Canvas>
+        <Scene />
+      </Canvas>
+    </main>
+  )
 }
